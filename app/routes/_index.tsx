52077f7824d53c4778801626668f59c37d7a9812
BrianDemorcy <501526@containerstore.com>
2023-08-18 23:57:57 -0400

    Styling update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -29,7 +29,7 @@ export default function Index() {
 					}}>
 					<div className='h-full w-full relative flex justify-center items-center lg:w-1/2'>
 						<div className='bg-dark h-full w-full mix-blend-multiply absolute'></div>
-						<div className='w-auto max-w-2xl md:max-w-lg	h-auto absolute grid gap-12 p-8'>
+						<div className='w-auto max-w-2xl md:max-w-lg	h-auto absolute grid gap-8 p-8'>
 							<h1 className='text-white font-bold '>
 								Selling
 								<br />
