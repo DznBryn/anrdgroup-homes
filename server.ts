e44cc969b45076aeb608a67dd0dfa3d9564df4c7
BrianDemorcy <501526@containerstore.com>
2023-08-23 20:05:43 -0400

    remove env local

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { logDevReady } from "@remix-run/cloudflare";
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 import * as build from "@remix-run/dev/server-build";
-import DEV_ENV from './env'
+
 if (process.env.NODE_ENV === "development") {
   logDevReady(build);
 }
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === "development") {
 export const onRequest = createPagesFunctionHandler({
   build,
   getLoadContext: (context) => {
-    return (process.env.NODE_ENV === "development") ? { env: { ...context.env, ...DEV_ENV} } :{ env: context.env }
+    return { env: context.env }
   },
   mode: process.env.NODE_ENV,
 
