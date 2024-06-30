d21ba9481d0a2074759d832303fa4db4118bf14d
BrianDemorcy <501526@containerstore.com>
2024-06-30 09:44:26 -0400

    Added footer copyright

--- app/components/Layouts/Footer.tsx
+++ app/components/Layouts/Footer.tsx
@@ -4,6 +4,8 @@ type Props = {}
 
 export default function Footer({}: Props) {
   return (
-    <div className='w-full h-auto m-0 mt-auto bg-dark p-12 text-white'></div>
-  )
+		<div className='w-full h-auto m-0 mt-auto bg-dark p-12 text-white'>
+			<p className='text-center text-white'> © Copyright {new Date().getFullYear()} | ANRD Group Inc.</p>
+		</div>
+	);
 }
