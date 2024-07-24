494065d2798c0d962969310000a781a90b2fecaa
BrianDemorcy <501526@containerstore.com>
2024-07-24 04:55:07 -0400

    update sitemap

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -1,23 +1,31 @@
-import { LoaderArgs } from "@remix-run/cloudflare";
+import { LoaderArgs } from '@remix-run/cloudflare';
 
-export function loader({request, context}: LoaderArgs) {
-  console.log({request, context})
-  // make this dynamic to routes
-  const urls = [
+export function loader({ request, context }: LoaderArgs) {
+	console.log({ request, context });
+	// make this dynamic to routes
+	const urls = [
 		`<url>
       <loc>https://anrdhomes.com/</loc>
       <changefreq>daily</changefreq>
       <priority>1.0</priority>   
     </url>`,
 	];
-  return new Response(`
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-      ${urls.join('')}
+	return new Response(
+		`<urlset 
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+        <url>
+          <loc>https://anrdhomes.com/</loc>
+          <changefreq>daily</changefreq>
+          <priority>1.0</priority>   
+        </url>
       <urlset>
-    `,{
-    status: 200,
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  })
+    `,
+		{
+			status: 200,
+			headers: {
+				'Content-Type': 'application/xml',
+			},
+		}
+	);
 }
